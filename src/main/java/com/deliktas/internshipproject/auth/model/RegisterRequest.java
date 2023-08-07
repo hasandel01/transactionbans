4ca070ee67e96b32b44efa747010e574f0ec3a0c
hasandel01 <109554308+hasandel01@users.noreply.github.com>
2023-08-07 17:38:45 +0300

    Tests are resolved and new crud operations implemented. After the presentation.

--- src/main/java/com/deliktas/internshipproject/auth/model/RegisterRequest.java
+++ src/main/java/com/deliktas/internshipproject/auth/model/RegisterRequest.java
@@ -4,7 +4,9 @@ package com.deliktas.internshipproject.auth.model;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
+@Component
 @Getter
 @Setter
 @RequiredArgsConstructor
--- src/main/java/com/deliktas/internshipproject/auth/model/Role.java
+++ src/main/java/com/deliktas/internshipproject/auth/model/Role.java
@@ -1,5 +1,8 @@
 package com.deliktas.internshipproject.auth.model;
 
+
+import org.springframework.stereotype.Component;
+
 public enum Role {
 
     USER,
